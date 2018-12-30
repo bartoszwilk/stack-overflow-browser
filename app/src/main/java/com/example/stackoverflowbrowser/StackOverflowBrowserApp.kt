6e2c75c50f0f0c7acb992a2176cc1ca3317373a7
Bartosz Wilk <bartosz.wilk@grupawp.pl>
2018-12-30 21:55:45 +0100

    [di] add: koin deps & initialization

--- app/src/main/java/com/example/stackoverflowbrowser/StackOverflowBrowserApp.kt
+++ app/src/main/java/com/example/stackoverflowbrowser/StackOverflowBrowserApp.kt
@@ -1,10 +1,13 @@
 package com.example.stackoverflowbrowser
 
 import android.app.Application
+import com.example.stackoverflowbrowser.di.issuesSearchModule
+import org.koin.android.ext.android.startKoin
 
-class StackOverflowBrowserApp: Application() {
+class StackOverflowBrowserApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        startKoin(this, listOf(issuesSearchModule))
     }
 }
