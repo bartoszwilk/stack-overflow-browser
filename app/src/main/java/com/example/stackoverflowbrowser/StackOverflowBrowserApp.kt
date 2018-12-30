e880c672622cf9b1cdd65b557a09f9f853e45862
Bartosz Wilk <bartosz.wilk@grupawp.pl>
2018-12-30 17:44:52 +0100

    [general] add: custom application class

---
+++ app/src/main/java/com/example/stackoverflowbrowser/StackOverflowBrowserApp.kt
@@ -0,0 +1,10 @@
+package com.example.stackoverflowbrowser
+
+import android.app.Application
+
+class StackOverflowBrowserApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
