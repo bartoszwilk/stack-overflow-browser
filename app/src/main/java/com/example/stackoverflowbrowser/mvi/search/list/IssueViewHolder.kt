7b62e24e700eb6ec06d9702cc88a071c114e21fd
Bartosz Wilk <bartosz.wilk@grupawp.pl>
2018-12-31 13:00:40 +0100

    [issues-search] add: issue viewholder

---
+++ app/src/main/java/com/example/stackoverflowbrowser/mvi/search/list/IssueViewHolder.kt
@@ -0,0 +1,19 @@
+package com.example.stackoverflowbrowser.mvi.search.list
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.viewholder_issue.view.*
+
+class IssueViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(item: IssueItem) {
+        val issue = item.issue
+        with(itemView) {
+            title.text = issue.title
+            answersCount.text = issue.answersCount.toString()
+            username.text = issue.owner.name
+            Glide.with(itemView).load(issue.owner.avatarUrl).into(avatar)
+        }
+    }
+}
