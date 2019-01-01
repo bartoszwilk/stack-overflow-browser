48c747277779eb4afb809e5155fa553cc5614ae4
Bartosz Wilk <bartosz.wilk@grupawp.pl>
2019-01-02 00:54:09 +0100

    [issue-details] add: toolbar backing arrow

--- app/src/main/java/com/example/stackoverflowbrowser/util/web/WebActivity.kt
+++ app/src/main/java/com/example/stackoverflowbrowser/util/web/WebActivity.kt
@@ -2,6 +2,7 @@ package pl.wp.videostar.viper.web
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.example.stackoverflowbrowser.R
 import kotlinx.android.synthetic.main.activity_web.*
 
@@ -13,7 +14,12 @@ class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         webView.loadUrl(url)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId == android.R.id.home) finish()
+        return super.onOptionsItemSelected(item)
+    }
 }
